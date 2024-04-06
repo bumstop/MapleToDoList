20b3abadcc398532ff883809ad73ba53fb25a302
bumstop <bums1290@gmail.com>
2024-04-07 02:36:21 +0900

    start project

---
+++ my-app/src/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
+import { Main } from "./main";
+
+export function Layout() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
