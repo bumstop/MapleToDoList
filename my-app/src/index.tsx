eaee16dc099a9086740e049f7346694647b1988f
bumstop <bums1290@gmail.com>
2024-04-18 07:26:22 +0900

    Update index.tsx

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./layout/Layout";
 import { Home } from "./pages/Home";
